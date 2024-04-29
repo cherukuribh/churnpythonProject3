69a519c4d3bbb8f7029c3832d563cce239e77999
cherukuribh <cherukuri.barathi@gmail.com>
2024-04-29 20:03:51 +0100

    updated

--- newprediction.py
+++ newprediction.py
@@ -2,9 +2,7 @@ from pyspark.sql import SparkSession
 
 # Initialize SparkSession
 spark = SparkSession.builder \
-    .appName("Prediction") \
-  #  .master("local[1]") \
-    .getOrCreate()
+    .appName("Prediction").getOrCreate()
 
 accounts_df = spark.read.format("jdbc").option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb").option("dbtable", "accounts_table").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").load()
 customers_df = spark.read.format("jdbc").option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb").option("dbtable", "customers_table").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").load()
