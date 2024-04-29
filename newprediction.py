faf04d3c937d64665a263654e2e5e223878d68b6
cherukuribh <cherukuri.barathi@gmail.com>
2024-04-29 20:01:45 +0100

    updated

---
+++ newprediction.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+
+# Initialize SparkSession
+spark = SparkSession.builder \
+    .appName("Prediction") \
+  #  .master("local[1]") \
+    .getOrCreate()
+
+accounts_df = spark.read.format("jdbc").option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb").option("dbtable", "accounts_table").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").load()
+customers_df = spark.read.format("jdbc").option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb").option("dbtable", "customers_table").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").load()
+
+transactions_df = spark.read.format("jdbc").option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb").option("dbtable", "transactions_table").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").load()
+
+# Join DataFrames
+new_accounts_df = accounts_df.join(customers_df, "Customer_ID")
+new_DF = new_accounts_df.join(transactions_df, "Account_ID")
+
+# Show the resulting DataFrame
+new_DF.show()
