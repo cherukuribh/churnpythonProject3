e39ebaece26a3e19b44bb2dac880204ccf2b305c
cherukuribh <cherukuri.barathi@gmail.com>
2024-04-30 10:08:49 +0100

    updated

--- pred.py
+++ pred.py
@@ -20,6 +20,11 @@ customers_df = spark.read.format("jdbc").option("url", "jdbc:postgresql://ec2-3-
 
 transactions_df = spark.read.format("jdbc").option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb").option("dbtable", "transactionstable").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").load()
 
+accounts_df = accounts_df.toPandas()
+customers_df = customers_df.toPandas()
+transactions_df = transactions_df.toPandas()
+
+
 # Join DataFrames
 new_accounts_df = accounts_df.join(customers_df, "Customer_ID")
 final_merged_df = new_accounts_df.join(transactions_df, "Account_ID")
